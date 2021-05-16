12e006c1592a5f637fe05147ed23dfed000983cb
Jon <jongold00@gmail.com>
2021-05-16 12:30:09 -0400

    Work on XP Particles

---
+++ Assets/particleshit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
+
+public class particleshit : MonoBehaviour
+{
+
+    public ParticleSystem particlesSystem;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Particle[]
+    }
+}
