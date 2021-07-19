bb2df716c3a0dbec73efba88ca7640f59b5590e9
Jon <jongold00@gmail.com>
2021-07-19 13:15:48 -0400

    Automata Protoype p2

--- Assets/Jon/Level Generation/LevelGenerator.cs
+++ Assets/Jon/Level Generation/LevelGenerator.cs
@@ -6,36 +6,34 @@ using UnityEditor;
 
 public class LevelGenerator : MonoBehaviour
 {
+
+
     [Range(0, 100)]
-    public int initChance;
-
+    public int iniChance;
     [Range(1, 8)]
-    public int birthLimit;      // if a cell has fewer than this many neighbors, it will not be born
-
+    public int birthLimit;
     [Range(1, 8)]
-    public int deathLimit;      // if a cell has fewer than this many neighbors, it will stay dead
+    public int deathLimit;
 
-    [Range(1,10)]
-    public int cycles;
+    [Range(1, 10)]
+    public int numR;
+    private int count = 0;
 
-    private int count;
     private int[,] terrainMap;
-    public Vector3Int tmapSize;
-
+    public Vector3Int tmpSize;
     public Tilemap topMap;
-    public Tilemap bottomMap;
-
+    public Tilemap botMap;
     public Tile topTile;
-    public Tile bottomTile;
+    public Tile botTile;
 
     int width;
     int height;
 
-    public void DoSim(int numReps)
+    public void doSim(int nu)
     {
         clearMap(false);
-        width = tmapSize.x;
-        height = tmapSize.y;
+        width = tmpSize.x;
+        height = tmpSize.y;
 
         if (terrainMap == null)
         {
@@ -43,7 +41,8 @@ public class LevelGenerator : MonoBehaviour
             initPos();
         }
 
-        for (int i = 0; i < numReps; i++)
+
+        for (int i = 0; i < nu; i++)
         {
             terrainMap = genTilePos(terrainMap);
         }
@@ -52,63 +51,16 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                if (terrainMap[x,y] == 1)
-                {
+                if (terrainMap[x, y] == 1)
                     topMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), topTile);
-                    bottomMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), bottomTile);
-                }
                 if (terrainMap[x, y] == 0)
                 {
-                    bottomMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), bottomTile);
-                }
-
-            }
-        }
-    }
-
-    public int [,] genTilePos(int[,] oldMap)
-    {
-        int[,] newMap = new int[width, height];
-        int numNeighbors;
-        BoundsInt bounds = new BoundsInt(-1, -1, 0, 3, 3, 1);
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                numNeighbors = 0;
-                foreach (var curr in bounds.allPositionsWithin)
-                {
-                    if (curr.x == 0 && curr.y == 0) continue;
-                    if (x + curr.x >= 0 && x + curr.x < width && y + curr.y > 0 && y + curr.y < height)
-                    {
-                        numNeighbors += oldMap[x + curr.x, y + curr.y];
-                    }
-                    else
-                    {
-                        numNeighbors++;
-                    }
-                }
-                if (oldMap[x,y] == 1)
-                {
-                    if (numNeighbors < deathLimit) newMap[x, y] = 0;
-                    else
-                    {
-                        newMap[x, y] = 1;
-                    }
-        
-                }
-                if (oldMap[x, y] == 0)
-                {
-                    if (numNeighbors < birthLimit) newMap[x, y] = 1;
-                    else
-                    {
-                        newMap[x, y] = 0;
-                    }
+                    botMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), botTile);
                 }
             }
         }
-        return newMap;
+
+
     }
 
     public void initPos()
@@ -117,34 +69,138 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                terrainMap[x, y] = Random.Range(1, 101) < initChance ? 1 : 0;
+                terrainMap[x, y] = Random.Range(1, 101) < iniChance ? 1 : 0;
             }
+
         }
+
     }
 
 
-    // Update is called once per frame
+    public int[,] genTilePos(int[,] oldMap)
+    {
+        int[,] newMap = new int[width, height];
+        int neighb;
+        BoundsInt myB = new BoundsInt(-1, -1, 0, 3, 3, 1);
+
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                neighb = 0;
+                foreach (var b in myB.allPositionsWithin)
+                {
+                    if (b.x == 0 && b.y == 0) continue;
+                    if (x + b.x >= 0 && x + b.x < width && y + b.y >= 0 && y + b.y < height)
+                    {
+                        neighb += oldMap[x + b.x, y + b.y];
+                    }
+                    else
+                    {
+                        neighb++;
+                    }
+                }
+
+                if (oldMap[x, y] == 1)
+                {
+                    if (neighb < deathLimit) newMap[x, y] = 0;
+
+                    else
+                    {
+                        newMap[x, y] = 1;
+
+                    }
+                }
+
+                if (oldMap[x, y] == 0)
+                {
+                    if (neighb > birthLimit) newMap[x, y] = 1;
+
+                    else
+                    {
+                        newMap[x, y] = 0;
+                    }
+                }
+
+            }
+
+        }
+
+
+
+        return newMap;
+    }
+
+
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            DoSim(cycles);
+            doSim(numR);
         }
+
 
         if (Input.GetMouseButtonDown(1))
         {
             clearMap(true);
         }
+
+
+
+        if (Input.GetMouseButton(2))
+        {
+            SaveAssetMap();
+            count++;
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+    public void SaveAssetMap()
+    {
+        string saveName = "tmapXY_" + count;
+        var mf = GameObject.Find("Grid");
+
+        if (mf)
+        {
+            var savePath = "Assets/" + saveName + ".prefab";
+            if (PrefabUtility.CreatePrefab(savePath, mf))
+            {
+                EditorUtility.DisplayDialog("Tilemap saved", "Your Tilemap was saved under" + savePath, "Continue");
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Tilemap NOT saved", "An ERROR occured while trying to saveTilemap under" + savePath, "Continue");
+            }
+
+
+        }
+
+
     }
 
     public void clearMap(bool complete)
     {
-        topMap.ClearAllTiles();
-        bottomMap.ClearAllTiles();
 
+        topMap.ClearAllTiles();
+        botMap.ClearAllTiles();
         if (complete)
         {
             terrainMap = null;
         }
+
+
     }
+
+
+
 }
