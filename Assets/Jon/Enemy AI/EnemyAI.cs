61895b9d971adaa43cee73539bb39bb4bc15c2c4
Jon <jongold00@gmail.com>
2021-08-03 11:26:21 -0400

    AI Prototype 2, flee semi working

--- Assets/Jon/Enemy AI/EnemyAI.cs
+++ Assets/Jon/Enemy AI/EnemyAI.cs
@@ -11,29 +11,50 @@ public class EnemyAI : MonoBehaviour
         Pursuing
     }
 
+    public Vector3 spawnPoint = new Vector3(0, 0, 0);
+
+    public GameObject player;
+
+    Vector3 playerPos;
+
+    Rigidbody2D rb2d;
+
+    public Vector3 currentDestination;
+
     public State currentState;
+
+
 
     public float wanderMoveSpeed;
     public float wanderDistance;
 
-    public Vector3 spawnPoint = new Vector3(0, 0, 0);
-
-    Rigidbody2D rb2d;
-    public Vector3 currentDestination;
     float timeBeforeNewWander;
+
+    public float fleeMoveSpeed;
+    public float fleeDistance;
+
+
+
+
 
 
     private void Start()
     {
+        player = FindObjectOfType<PlayerController>().gameObject;
         rb2d = GetComponent<Rigidbody2D>();
-        currentState = State.Wandering;
+        currentState = State.Fleeing;
         ChooseNewDestination();
+
+        
+
+
+
 
     }
 
     private void Update()
     {
-
+        playerPos = player.transform.position;
 
         switch (currentState)
         {
@@ -72,12 +93,40 @@ public class EnemyAI : MonoBehaviour
 
     void UpdateFleeing()
     {
-
+        if (ArrivedAtDestination())
+        {
+            timeBeforeNewWander -= Time.deltaTime;
+            rb2d.velocity = new Vector2(0, 0);
+            if (timeBeforeNewWander <= 0.0f)
+            {
+                timeBeforeNewWander = Random.Range(1, 3);
+                ChooseNewDestination();
+            }
+        }
+        else
+        {
+            rb2d.velocity = fleeMoveSpeed * ((currentDestination - transform.position).normalized);
+            // 
+        }
     }
 
     void UpdatePursuing()
     {
-
+        if (ArrivedAtDestination())
+        {
+            timeBeforeNewWander -= Time.deltaTime;
+            rb2d.velocity = new Vector2(0, 0);
+            if (timeBeforeNewWander <= 0.0f)
+            {
+                timeBeforeNewWander = Random.Range(1, 3);
+                ChooseNewDestination();
+            }
+        }
+        else
+        {
+            rb2d.velocity = wanderMoveSpeed * (Mathf.Clamp(Vector3.Distance(transform.position, currentDestination) * wanderDistance / 2, 0.5f, 1.0f)) * ((currentDestination - transform.position).normalized);
+            // 
+        }
     }
 
     bool ArrivedAtDestination()
@@ -94,8 +143,20 @@ public class EnemyAI : MonoBehaviour
         switch(currentState)
         {
             case State.Wandering: // choose a random point that is in the circle of radius wanderDistance located at spawnPoint position
-                currentDestination = new Vector3(spawnPoint.x + Random.Range(wanderDistance * -1, wanderDistance), spawnPoint.y + Random.Range(wanderDistance * -1, wanderDistance));   
+                Vector3 wanderDest;
+                wanderDest = new Vector3(spawnPoint.x + Random.Range(wanderDistance * -1, wanderDistance), spawnPoint.y + Random.Range(wanderDistance * -1, wanderDistance));
+                currentDestination = wanderDest;
                 break;
+
+            case State.Fleeing: // choose a random point away from player, slightly to a side at random
+                Vector3 fleeDest;
+                float angleFromPlayer = Mathf.Deg2Rad * Mathf.Atan((transform.position.y - playerPos.y)/(transform.position.x - playerPos.x));    // directional vector from player pointing at AI unit
+                float alteredAngle = Mathf.Tan(angleFromPlayer + Random.Range(-20, 20));
+                fleeDest = new Vector3(Mathf.Cos(alteredAngle), Mathf.Sin(alteredAngle), 0) * (Vector3.Distance(transform.position, playerPos) + fleeDistance);
+                currentDestination = fleeDest;
+                break;
+
         }
+
     }
 }
